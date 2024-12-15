8ebfde7886d1bcc599fe48d1671bcfb7c621395b
tgorman31 <gorman.thomas31@gmail.com>
2024-12-15 19:16:05 +1300

    Day_3 part 2

--- Day_3/main.go
+++ Day_3/main.go
@@ -15,25 +15,43 @@ func check(e error) {
 }
 
 func main() {
-	// str := "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+	str := "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 	dat, err := os.ReadFile("dataP1.txt")
 	check(err)
-
+	input := ""
 	stringifyDat := string(dat)
 
-	// mul(44,46) 1-3 digit numbers
-	r, _ := regexp.Compile(`mul\([0-9]{1,3}\,[0-9]{1,3}\)`)
+	test := false
+	include := true
+
+	if test {
+		input = str
+	} else {
+		input = stringifyDat
+	}
+
+	r, _ := regexp.Compile(`(?:mul\([0-9]{1,3}\,[0-9]{1,3}\))|(?:do\(\))|(?:don\'t\(\))`)
 	r2, _ := regexp.Compile(`[0-9]{1,3}\,[0-9]{1,3}`)
 	total := int64(0)
 
-	for _, match := range r.FindAllString(stringifyDat, -1) {
-		// fmt.Println(match, "found at index", i)
-		match2 := r2.FindString(match)
-		num := s.Split(match2, ",")
-		val1, _ := strconv.ParseInt(num[0], 0, 64)
-		val2, _ := strconv.ParseInt(num[1], 0, 64)
-		product := val1 * val2
-		total = total + product
+	for _, match := range r.FindAllString(input, -1) {
+
+		switch match {
+		case "don't()":
+			include = false
+		case "do()":
+			include = true
+		default:
+			if include {
+				match2 := r2.FindString(match)
+				num := s.Split(match2, ",")
+				val1, _ := strconv.ParseInt(num[0], 0, 64)
+				val2, _ := strconv.ParseInt(num[1], 0, 64)
+				product := val1 * val2
+				total = total + product
+			}
+
+		}
 	}
 
 	fmt.Println(total)
